d3ebb85bb125bdeffd873c96e1fa705c4e7b233a
Dmitry Laabe <dmitry@it-dominanta.com>
2025-06-05 19:07:23 +0300

    3 commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const hostname = '0.0.0.0';
-const port = 3060;
+const port = 3061;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World');
+res.end('Hello World 2!');
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
